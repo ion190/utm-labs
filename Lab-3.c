effd7bfe7fa0986c3d2a04bb30e915ed4591398a
none190 <145373308+none190@users.noreply.github.com>
2023-10-03 10:53:15 +0300

    Update Lab-3.c

--- Lab-3.c
+++ Lab-3.c
@@ -145,3 +145,92 @@ void quickSort(int arr[], int start, int end) {
         a = b;
         b = t;
     }
+
+
+
+
+
+
+
+
+
+
+
+
+#include <stdio.h>
+#include <time.h>
+#include <stdbool.h>
+
+// Function prototypes
+int* sort2DMatrix(int arr[][100], int n, int m);
+void mountainSort(int oldArr[], int newArr[][100], int n, int m);
+int* sortQuick(int arr[], int start, int end);
+
+int main() {
+    int n, m; // lengths of array
+    printf("n (row) = ");
+    scanf("%d", &n);
+    printf("m (column) = ");
+    scanf("%d", &m);
+    
+    int arr[n][m];
+    printf("\nArray:\n");
+    for(int i = 0; i < n; i++) {
+        printf("Row %d:\n", i + 1);
+        for(int j = 0; j < m; j++) {
+            printf("Element %d = ", j + 1);
+            scanf("%d", &arr[i][j]);
+        }
+    }
+
+    int* sortedMatrix1D = sort2DMatrix(arr, n, m);
+    int sortedMatrix2D[n][m];
+
+    mountainSort(sortedMatrix1D, sortedMatrix2D, n, m);
+
+    printf("\nSorted Array:\n");
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < m; j++) {
+            printf("%d ", sortedMatrix2D[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
+
+// Sorts a 2D matrix into a 1D array using quicksort
+int* sort2DMatrix(int arr[][100], int n, int m) {
+    int* sorted1D = (int*)malloc(sizeof(int) * n * m);
+    int index = 0;
+
+    // Push all elements from arr to sorted1D
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < m; j++) {
+            sorted1D[index] = arr[i][j];
+            index++;
+        }
+    }
+
+    sortQuick(sorted1D, 0, n * m);
+
+    return sorted1D;
+}
+
+// Sorts a 1D array using quicksort
+int* sortQuick(int arr[], int start, int end) {
+    if (start < end) {
+        // Find the pivot element such that elements smaller than pivot are on the left, elements greater than pivot are on the right
+        int p = partition(arr, start, end);
+
+        // Recursive call on the left of pivot
+        sortQuick(arr, start, p - 1);
+        // Recursive call on the right of pivot
+        sortQuick(arr, p + 1, end);
+    }
+
+    return arr;
+}
+
+// Rest of your code remains unchanged...
+
