7e6673e0a4b12d99376d58f57c3dd5c66273177c
none190 <145373308+none190@users.noreply.github.com>
2023-10-15 22:17:35 +0300

    Update Lab-4.c

--- Lab-4.c
+++ Lab-4.c
@@ -68,10 +68,6 @@ int main() {
 
     // Medium - function that reads a sentence from the user and counts the number of punctuation marks (e.g., periods, commas, question marks) in the sentence
     printf("\nThere are %d punctuation marks\n", countPunctuationMarks(text, strlen(text)));
-
-    // Medium - function that takes a sentence as input and returns the sentence with words sorted alphabetically
-    // sortText(text, strlen(text));
-    // printf("\nSorted text alphabetically: %s", text, wordsLength);
     
     // Medium - function that reads a string from the user and checks if it is a valid email address based on simple criteria (e.g., contains ”@” and ”.” symbols in the right places)
     char email[1000];
